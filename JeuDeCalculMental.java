103f2c6a6636f2daa1c238d9d4d3580a5675deea
Dakusy <32799638+Dakusy@users.noreply.github.com>
2017-12-21 09:57:03 +0100

    add change Projet v0.1

--- JeuDeCalculMental.java
+++ JeuDeCalculMental.java
@@ -1,40 +1,22 @@
+import extensions.CSVFile;
+import extensions.Image;
+import extensions.RGBColor;
 class JeuDeCalculMental extends Program {
+    boolean finished = false;
 
     int operande1=operande();
     int operande2=operande();
 
-    void testInitialiser(){
-	assertArrayEquals(new int[][]{
-		{0,0,0},
-		{0,0,0},
-		{0,0,0}},initialiser());
-    }
-
-    int[][] initialiser(){
-	int[][] tab=new int[3][3];
-	for(int i=0; i<length(tab,1); i++){
-	    for(int j=0; j<length(tab,2); j++){
-		tab[i][j]=0;
-	    }
-	}
-	return tab;
-    }
-    
+    //Calcul les différentes opérandes.
     int operande(){
 	int nombre=(int)(random()*100);
 	return nombre;
     }
-
-    void testAffichage(){
-	assertEquals("2 + 5", affichage(2,5));
-    }
-
-    String affichage(int operande1, int operande2){
-	String res=operande1 + " + " + operande2;
-	return res;
-    }
-
-    int[][] tableauEntier(int[][] tab){
+    
+    
+    //Fonction permettant d'initialiser le jeu. 
+    int[][] initialiser(){
+	int[][] tab=new int[3][3];
 	for(int i=0; i<length(tab,1); i++){
 	    for(int j=0; j<length(tab,2); j++){
 		tab[i][j]=operande();
@@ -44,6 +26,17 @@ class JeuDeCalculMental extends Program {
 	return tab;
     }
 
+    //Test l'Affichage des opérandes.
+    void testAffichage(){
+	assertEquals("2 + 5", affichage(2,5));
+    }
+    //Affiche les opérandes dans le jeu.
+    String affichage(int operande1, int operande2){
+	String res=operande1 + " + " + operande2;
+	return res;
+    }
+
+    //Test l'affichage du tableau d'entier.
     void testAffichageTableau(){
 	int[][] tab = new int[][]{
 	    {1,2,3},
@@ -52,7 +45,7 @@ class JeuDeCalculMental extends Program {
 	};
 	assertEquals(" 1 2 3\n 4 5 6\n 7 8 9\n",affichageTableau(tab));
     }
-
+     //Affiche le tableau en chaîne de caractère.
     String affichageTableau(int[][] tab){
 	String res="";
 	for(int i=0; i<length(tab,1); i++){
@@ -63,11 +56,83 @@ class JeuDeCalculMental extends Program {
 	}
 	return res;
     }
+    //Test la fonction saisie
+   void testSaisie(){
+	int[][] tab = new int[][]{
+	    {10,25,3},
+	    {42,5,76},
+	    {12,89,100}
+	};
+	assertTrue(saisie(1,2,tab));
+	assertTrue(saisie(2,0,tab));
+	assertFalse(saisie(3,4,tab));
+    }
+    //Vérifie que la saisie des coordonnées soit dans le tableau.
+    boolean saisie(int a, int b, int[][] tab){
+	boolean res=false;
+	if(a<=length(tab,1) && b<=length(tab,2)){
+	    res=true;
+	}
+	return res;
+    }
+    //Test la fonction bonneReponse.
+       void testBonneReponse(){
+	int[][] tab = new int[][]{
+	    {10,25,3},
+	    {42,5,76},
+	    {12,89,100}
+	};
+	assertEquals(true, bonneReponse(1,2,tab,50,26));
+    }
+    //Vérifie que l'entier sélectionner dans le tableau soit le résultat des 2 opérandes.
+    boolean bonneReponse(int l, int c, int[][] tab, int operande1, int operande2){
+	boolean res=false;
+	if(tab[l][c]==operande1+operande2){
+	    res=true;
+	}
+	return res;
+    }
+    
+    /*  void keyTypedInConsole(char key) {
+    	switch(key) {
+    	case ANSI_UP:
+    	    println("haut");
+    	    break;
+    	case ANSI_DOWN:
+    	    println("bas");
+    	    break;
+    	case ANSI_LEFT:
+    	    println("gauche");
+    	    break;
+    	case ANSI_RIGHT:
+    	    println("droite");
+    	    break;
+    	case 'q':
+    	    println("quitter");
+    	    finished = true;
+	    }*/
 
+	
+
+    
+    
+    //Execute les fonctions pour donner le jeu.
     void algorithm(){
-	println(affichage(operande1, operande2));
+	int[][] plateau=initialiser();
+	int tours=0;
 	println();
-	println(affichageTableau(tableauEntier(initialiser())));
+	println("Combien font: " + affichage(operande1, operande2));
+	println();
+	println(affichageTableau(plateau));
+	println("Veuillez saisir un indice de ligne: ");
+	int l=readInt();
+	println("Veuillez saisir un indice de colonne: ");
+	int c=readInt();
+	while(saisie(l,c,plateau)!=true){
+	    println("Veuillez saisir un indice de ligne correct: ");
+	    l=readInt();
+	    println("Veuillez saisir un indice de colonne correct: ");
+	    c=readInt();
+	}
     }
 }
-
